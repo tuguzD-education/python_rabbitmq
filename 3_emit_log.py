5a41905956a764eb93cc74e7ccc6d1a0d9b45f57
tuguzD <0damir.1tugushev@gmail.com>
2023-01-26 14:45:27 +0300

    Initial commit

---
+++ 3_emit_log.py
@@ -0,0 +1,15 @@
+import pika, sys
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(
+    exchange='logs', exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or b"info: Hello World!"
+channel.basic_publish(
+    exchange='logs', routing_key='', body=message)
+print(" [x] Sent %r" % message)
+
+connection.close()
---
+++ 3_receive_logs.py
@@ -0,0 +1,23 @@
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(
+    exchange='logs', exchange_type='fanout')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(
+    queue=queue_name, auto_ack=True,
+    on_message_callback=callback)
+
+channel.start_consuming()
+print(' [*] Waiting for logs. To exit press CTRL+C')
